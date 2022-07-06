eed0ff73fcd21ee43a9b796fb9972d9a31bd2082
Cayden Lords <lor20002@byui.edu>
2022-07-06 09:22:25 -0600

    Scene Constants

---
+++ Constants.cs
@@ -0,0 +1,6 @@
+
+//SCENES
+public static string NEW_GAME = "new_game";
+public static string CAMERA_1 = "camera_1";
+public static string OFFICE = "office";
+public static string GAME_OVER = "game_over";
