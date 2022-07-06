77f6d2e0a91a80888d24ae7c6048944c0f6eff21
Cayden Lords <lor20002@byui.edu>
2022-07-06 09:28:19 -0600

    More constants and housekeeping

--- Constants.cs
+++ Constants.cs
@@ -1,6 +1,12 @@
-
+//GAME
+public static string GAME_NAME = "Security";
+public static int FRAME_RATE = 60;
 //SCENES
 public static string NEW_GAME = "new_game";
 public static string CAMERA_1 = "camera_1";
 public static string OFFICE = "office";
 public static string GAME_OVER = "game_over";
+// TEXT
+public static int ALIGN_LEFT = 0;
+public static int ALIGN_CENTER = 1;
+public static int ALIGN_RIGHT = 2;
