2948fe3484b00fc8bef74b5ea65817c2d9f69120
Cayden Lords <lor20002@byui.edu>
2022-07-11 09:22:02 -0600

    Added Robot.cs

---
+++ Game/Casting/Robot.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace Unit06.Game.Casting
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Robot : Actor
+    {
+        private static Random random = new Random();
+
+        private Body body;
+        private Image image;
+
+        /// <summary>
+        /// Constructs a new instance of Actor.
+        /// </summary>
+        public Ball(Body body, Image image, bool debug = false) : base(debug)
+        {
+            this.body = body;
+            this.image = image;
+        }
+        
+        /// <summary>
+        /// Gets the body.
+        /// </summary>
+        /// <returns>The body.</returns>
+        public Body GetBody()
+        {
+            return body;
+        }
+
+        /// <summary>
+        /// Gets the image.
+        /// </summary>
+        /// <returns>The image.</returns>
+        public Image GetImage()
+        {
+            return image;
+        }
+    }
+}
