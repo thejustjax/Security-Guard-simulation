bd2639c38f4e58736d5c7435614951c32bc1696c
Jax McKinney <mck20018@byui.edu>
2022-07-11 16:15:39 -0600

    pls

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿using System;
+using Security.Game.Directing;
+using Security.Game.Services;
 
+namespace Security
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Director director = new Director(SceneManager.VideoService);
+            director.StartGame();
+        }
+    }
+}
