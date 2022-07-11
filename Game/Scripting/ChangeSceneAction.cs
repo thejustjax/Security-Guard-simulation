d8d58111a188b2a5e586e08552bfc964d13e7b63
BrennonL <lan18022@byui.edu>
2022-07-11 16:25:27 -0600

    if statements for buttons clicked

--- Game/Scripting/ChangeSceneAction.cs
+++ Game/Scripting/ChangeSceneAction.cs
@@ -21,6 +21,26 @@ namespace Security.Game.Scripting
             {
                 callback.OnNext(nextScene);
             }
+
+            if (keyboardService.IsKeyPressed(Constants.OFFICE_BUTTON))
+            {
+                
+            }
+
+            if (keyboardService.IsKeyPressed(Constants.ROOM1_BUTTON))
+            {
+
+            }
+
+            if (keyboardService.IsKeyPressed(Constants.ROOM2_BUTTON))
+            {
+
+            }
+
+            if (keyboardService.IsKeyPressed(Constants.HWALL_BUTTON))
+            {
+                
+            }
         }
 
         //<summery>
@@ -28,3 +48,11 @@ namespace Security.Game.Scripting
         //</summery>
     }
 }
+
+
+
+// public static string OFFICE_BUTTON = "z";
+// public static string ROOM1_BUTTON = "b";
+// public static string ROOM2_BUTTON = "v";
+// public static string HWALL_BUTTON = "c";
+// public static string EWALL_BUTTON = "x";
